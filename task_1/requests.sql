2266c38661c0972f3b69db571ccb400b7e3cd134
bigmasy <jura.tymosh@gmail.com>
2024-07-01 09:27:14 +0300

    hw

--- task_1/requests.sql
+++ task_1/requests.sql
@@ -1,26 +1,40 @@
-select * from  tasks where user_id = 1
-SELECT * FROM tasks WHERE status_id = (SELECT id FROM status WHERE name = 'new')
-UPDATE tasks SET status_id = (SELECT id FROM status WHERE name = 'in progress') WHERE id = 1;
-select * from users where id not in (select user_id from tasks)
-insert into tasks(title, description, status_id, user_id) values ('test', 'test1', 2, 5)
-select * from tasks where status_id != (select id from status where name = 'completed') 
-delete from tasks where id = 21
-select * from users where email like '%@example.org%'
-update users set fullname = 'new name' where id = 1
-SELECT s.name AS status, COUNT(t.id) AS task_count
-FROM status s
+--Отримати всі завдання певного користувача
+SELECT * FROM  tasks WHERE user_id = 1
+--Вибрати завдання за певним статусом
+SELECT * FROM tasks WHERE status_id = (SELECT id FROM STATUS WHERE NAME = 'new')
+--Оновити статус конкретного завдання
+UPDATE tasks SET status_id = (SELECT id FROM STATUS WHERE NAME = 'in progress') WHERE id = 1;
+--Отримати список користувачів, які не мають жодного завдання
+SELECT * FROM users WHERE id not in (SELECT user_id FROM tasks)
+--Додати нове завдання для конкретного користувача
+INSERT into tasks(title, description, status_id, user_id) values ('test', 'test1', 2, 5)
+--Отримати всі завдання, які ще не завершено
+SELECT * FROM tasks WHERE status_id != (SELECT id FROM STATUS WHERE NAME = 'completed') 
+--Видалити конкретне завдання
+delete FROM tasks WHERE id = 21
+--Знайти користувачів з певною електронною поштою
+SELECT * FROM users WHERE email like '%@example.org%'
+--Оновити ім'я користувача
+UPDATE users set fullname = 'new NAME' WHERE id = 1
+--Отримати кількість завдань для кожного статусу
+SELECT s.NAME AS STATUS, COUNT(t.id) AS task_count
+FROM STATUS s
 LEFT JOIN tasks t ON s.id = t.status_id
-GROUP BY s.name;
+GROUP BY s.NAME;
+--Отримати завдання, які призначені користувачам з певною доменною частиною електронної пошти
 SELECT t.*
 FROM tasks t
 JOIN users u ON t.user_id = u.id
 WHERE u.email LIKE '%@example.com';
+--Отримати список завдань, що не мають опису.
 SELECT * FROM tasks WHERE description IS NULL OR description = '';
+--Вибрати користувачів та їхні завдання, які є у статусі 'in progress'.
 SELECT u.fullname, t.title
 FROM users u
 INNER JOIN tasks t ON u.id = t.user_id
-INNER JOIN status s ON t.status_id = s.id
-WHERE s.name = 'in progress';
+INNER JOIN STATUS s ON t.status_id = s.id
+WHERE s.NAME = 'in progress';
+--Отримати користувачів та кількість їхніх завдань
 SELECT u.fullname, COUNT(t.id) AS task_count
 FROM users u
 LEFT JOIN tasks t ON u.id = t.user_id
---
+++ task_2/main.py
@@ -0,0 +1,110 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+# Підключення до локального MongoDB сервера або до MongoDB Atlas
+client = MongoClient('mongodb+srv://test:test@cluster0.7fvb0f9.mongodb.net/')
+
+# Вибір бази даних
+db = client['cats_db']
+
+# Вибір колекції
+cats_collection = db['cats']
+
+# Функція для додавання нового кота
+def create_cat(name, age, features):
+    cat = {
+        "name": name,
+        "age": age,
+        "features": features
+    }
+    cats_collection.insert_one(cat)
+    print(f"Кот {name} успішно доданий!")
+
+# Функція для виведення всіх записів із колекції
+def read_all_cats():
+    cats = cats_collection.find()
+    for cat in cats:
+        print(cat)
+
+# Функція для виведення інформації про кота за ім'ям
+def read_cat_by_name(name):
+    cat = cats_collection.find_one({"name": name})
+    if cat:
+        print(cat)
+    else:
+        print(f"Кота з ім'ям {name} не знайдено.")
+
+# Функція для оновлення віку кота за ім'ям
+def update_cat_age(name, new_age):
+    result = cats_collection.update_one({"name": name}, {"$set": {"age": new_age}})
+    if result.matched_count > 0:
+        print(f"Вік кота {name} успішно оновлений до {new_age}.")
+    else:
+        print(f"Кота з ім'ям {name} не знайдено.")
+
+# Функція для додавання нової характеристики до списку features кота за ім'ям
+def add_feature_to_cat(name, feature):
+    result = cats_collection.update_one({"name": name}, {"$push": {"features": feature}})
+    if result.matched_count > 0:
+        print(f"Характеристика '{feature}' успішно додана коту {name}.")
+    else:
+        print(f"Кота з ім'ям {name} не знайдено.")
+
+# Функція для видалення запису з колекції за ім'ям тварини
+def delete_cat_by_name(name):
+    result = cats_collection.delete_one({"name": name})
+    if result.deleted_count > 0:
+        print(f"Кота з ім'ям {name} успішно видалено.")
+    else:
+        print(f"Кота з ім'ям {name} не знайдено.")
+
+# Функція для видалення всіх записів із колекції
+def delete_all_cats():
+    result = cats_collection.delete_many({})
+    print(f"Всі коти успішно видалені. Видалено записів: {result.deleted_count}")
+
+# Основна функція для демонстрації роботи
+def main():
+    while True:
+        print("\nВиберіть дію:")
+        print("1. Додати кота")
+        print("2. Показати всіх котів")
+        print("3. Показати кота за ім'ям")
+        print("4. Оновити вік кота")
+        print("5. Додати характеристику коту")
+        print("6. Видалити кота за ім'ям")
+        print("7. Видалити всіх котів")
+        print("8. Вийти")
+
+        choice = input("Введіть номер дії: ")
+
+        if choice == '1':
+            name = input("Введіть ім'я кота: ")
+            age = int(input("Введіть вік кота: "))
+            features = input("Введіть характеристики кота (через кому): ").split(',')
+            create_cat(name, age, features)
+        elif choice == '2':
+            read_all_cats()
+        elif choice == '3':
+            name = input("Введіть ім'я кота: ")
+            read_cat_by_name(name)
+        elif choice == '4':
+            name = input("Введіть ім'я кота: ")
+            new_age = int(input("Введіть новий вік кота: "))
+            update_cat_age(name, new_age)
+        elif choice == '5':
+            name = input("Введіть ім'я кота: ")
+            feature = input("Введіть нову характеристику: ")
+            add_feature_to_cat(name, feature)
+        elif choice == '6':
+            name = input("Введіть ім'я кота: ")
+            delete_cat_by_name(name)
+        elif choice == '7':
+            delete_all_cats()
+        elif choice == '8':
+            break
+        else:
+            print("Невірний вибір. Спробуйте ще раз.")
+
+if __name__ == "__main__":
+    main()
